5463ed2618c3b3838e1cc36d052f628fe7739906
ThaZaathang <101632129@student.swin.edu>
2018-08-03 13:07:57 +1000

    Failing Unit Test

---
+++ ReleasePipeDemo/ReleasePipeDemoApp.Tests/Controllers/ExamplesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReleasePipeDemoApp;
+using ReleasePipeDemoApp.Controllers;
+
+namespace ReleasePipeDemoApp.Tests.Controllers
+{
+    [TestClass]
+    public class ExamplesControllerTests
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            ExamplesController controller = new ExamplesController();
+
+            // Act
+            IEnumerable<string> result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("value1", result.ElementAt(0));
+            Assert.AreEqual("value2", result.ElementAt(1));
+        }
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+            ExamplesController controller = new ExamplesController();
+
+            // Act
+            string result = controller.Get(5);
+
+            // Assert
+            Assert.AreEqual("wrong", result);
+        }
+    }
+}
